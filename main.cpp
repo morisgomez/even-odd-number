f2ee1292404126a48583d9a4ab9539df1fdef4ff
Moris Gomez <83724852+morisgomez@users.noreply.github.com>
2022-10-05 19:03:27 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+        cout << "Enter a whole number, and "
+                "we will tell you if it is "
+                "an even or odd number: ";
+        int userInput;
+        cin >> userInput;
+        int inputRemainder = userInput % 2;
+        if (inputRemainder == 0) {
+            cout << "Your number " << userInput << " is even" <<;
+        } else if (inputRemainder == 1) {
+            cout << "Your number " << userInput << " is odd.";
+        } else
+            cout << "Please enter a whole number." <<;
+    return 0;
+}
